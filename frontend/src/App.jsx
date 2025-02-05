43b3bbfa695440c9fa94b14b5362e95f932f684d
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-02-05 13:28:31 +0200

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <Button>Click Me</Button>
+      <Button>Signup</Button>
+  
+    </div>
+  )
+}
+
+export default App
+
